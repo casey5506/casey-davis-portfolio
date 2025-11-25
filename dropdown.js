65d0705f784bd040292a11668642f37a117cbdc1
casey5506 <casey5506@gmail.com>
2025-11-25 18:36:19 -0500

    Add files via upload
    version 1

---
+++ dropdown.js
@@ -0,0 +1,13 @@
+
+// DROP DOWN MENU
+
+// code from w3schools.com
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
